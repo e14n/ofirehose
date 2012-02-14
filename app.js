97c081c4819194464af78714d5017e3c7d9ab9e0
Evan Prodromou <evan@status.net>
2012-02-14 18:41:38 -0500

    some changes

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-
-/**
- * Module dependencies.
- */
+// main function for ofirehose.com
+//
+// Copyright 2012 StatusNet Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 var express = require('express')
   , routes = require('./routes');
@@ -30,6 +41,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.post('/ping', routes.ping);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,38 @@
+// main function for ofirehose.com
+//
+// Copyright 2012 StatusNet Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-/*
- * GET home page.
- */
+var cg = require('chain-gang'),
+    chain = cg.create({workers: 3});
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+exports.index = function(req, res) {
+    res.render('index', { title: 'Express' });
+};
+
+var distributor = function(activity) {
+    return function(job) {
+	job.finish(null);
+    };
+};
+
+exports.ping = function(req, res) {
+    var activity = req.body;
+    var callback = req.params.callback;
+
+    chain.add(distributor(activity), activity.id);
+
+    res.writeHead(201);
+    res.end();
 };
