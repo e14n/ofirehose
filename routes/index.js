71a10573fdc473c2cbc9c5955bb80fe8524ff6c7
Evan Prodromou <evan@status.net>
2012-02-21 13:25:50 -0500

    better location for libs

--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var theFeed = require('./lib/feed').theFeed,
-    localURL = require('./lib/url').localURL;
+var theFeed = require('../lib/feed').theFeed,
+    localURL = require('../lib/url').localURL;
 
 exports.index = function(req, res) {
     res.render('index', { title: 'Express' });
