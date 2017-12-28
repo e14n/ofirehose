5d5df5f7a00b98314854db77d2d1b92ebe86ac0d
AJ Jordan <alex@strugee.net>
2017-12-27 21:42:07 -0800

    Smoketest the app's HTTPS interface
    While we're at it, fix Travis so it runs as-root tests, too.

--- test/app-https-test-as-root.js
+++ test/app-https-test-as-root.js
@@ -22,64 +22,84 @@ var vows = require("perjury"),
     path = require("path"),
     databank = require("databank"),
     Step = require("step"),
-    http = require("http"),
     https = require("https"),
-    urlparse = require("url").parse;
+    urlparse = require("url").parse,
+    post = require("./data/post");
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 var suite = vows.describe("smoke test app interface over https");
 
-var httpsURL = function(url) {
-    var parts = urlparse(url);
-    return parts.protocol === "https:";
-};
-
 suite.addBatch({
-    "When we makeApp()": {
-        topic: function() {
-            var config = {port: 443,
-                          hostname: "secure.localhost",
-                          key: path.join(__dirname, "data", "secure.localhost.key"),
-                          cert: path.join(__dirname, "data", "secure.localhost.crt"),
-                          driver: "memory",
-                          params: {},
-                         },
-                makeApp = require("../lib/app");
+	"When we makeApp()": {
+		topic: function() {
+			var config = {port: 443,
+			              hostname: "localhost",
+			              key: path.join(__dirname, "data", "secure.localhost.key"),
+			              cert: path.join(__dirname, "data", "secure.localhost.crt"),
+			              driver: "memory",
+			              params: {}
+			             },
+			makeApp = require("../lib/app");
 
-            process.env.NODE_ENV = "test";
+			process.env.NODE_ENV = "test";
 
-            return makeApp(config);
-        },
-        "it works": function(err, app) {
-            assert.ifError(err);
-            assert.isObject(app);
-        },
-        "and we app.start()": {
-            topic: function(app) {
-                var cb = this.callback;
-                app.start(function(err) {
-                    if (err) {
-                        cb(err, null);
-                    } else {
-                        cb(null, app);
-                    }
-                });
-            },
-            teardown: function(app) {
-                if (app && app.server && app.server.close) {
-                    app.server.close(this.callback);
-                }
-            },
-            "it works": function(err, app) {
-                assert.ifError(err);
-            },
-            "app is listening on correct port": function(err, app) {
-                var addr = app.server.address();
-                assert.equal(addr.port, 443);
-            }
-        }
-    }
+			return makeApp(config);
+		},
+		"it works": function(err, app) {
+			assert.ifError(err);
+			assert.isObject(app);
+		},
+		"and we app.start()": {
+			topic: function(app) {
+				var cb = this.callback;
+				app.start(function(err) {
+					if (err) {
+						cb(err, null);
+					} else {
+						cb(null, app);
+					}
+				});
+			},
+			teardown: function(app) {
+				if (app && app.server && app.server.close) {
+					app.server.close(this.callback);
+				}
+			},
+			"it works": function(err, app) {
+				assert.ifError(err);
+			},
+			"app is listening on correct port": function(err, app) {
+				var addr = app.server.address();
+				assert.equal(addr.port, 443);
+			},
+			"and we POST to /ping": {
+				topic: function() {
+					var cb = this.callback,
+					    req = https.request({
+						    scheme: "http",
+						    host: "localhost",
+						    path: "/ping",
+						    port: 443,
+						    method: "POST"
+					    });
+
+					req.write(JSON.stringify(post));
+
+					req.on("error", cb);
+					req.on("response", function(res) {
+						cb(undefined, res);
+					});
+				},
+				"it works": function(err) {
+					assert.ifError(err);
+				},
+				"we get back HTTP 201": function(err, res) {
+					assert.equal(res.statusCode, 201);
+				}
+			}
+		}
+	}
 });
 
 suite["export"](module);
